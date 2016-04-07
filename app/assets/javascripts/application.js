2bfe84c57d0c041a2b0c16b1fbe0b6b4cfb79c92
Parth Patel <parth1634@gmail.com>
2016-04-07 18:00:17 +0530

    U

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,33 +1,15 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
-//
+//magazine
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-//= require bootstrap-sprockets
 //= require jquery/jquery-2.1.1.js
+//= require bootstrap-sprockets
+//= require metisMenu/jquery.metisMenu.js
 //= require pace/pace.min.js
+//= require peity/jquery.peity.min.js
 //= require slimscroll/jquery.slimscroll.min.js
 //= require inspinia.js
-//= require metisMenu/jquery.metisMenu.js
-//= require jeditable/jquery.jeditable.js
-//= require dataTables/datatables.min.js
-//= require peity/jquery.peity.min.js
-//= require peity/peity-demo.js
-//= require sweet-alert
-//= require custom
-
-
-
-
-
+//= require iCheck/icheck.min.js
