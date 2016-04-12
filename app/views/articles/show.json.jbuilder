583cd36b63c81cdf671bdb1f61aba9d48710dd35
Parth Patel <parth1634@gmail.com>
2016-04-13 01:32:27 +0530

    Articlea sre generated

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :body, :githubprofile, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles
+  resources :articles
+  resources :articles
+  resources :articles
   resources :repositories do
     collection do
       get 'purches/:id' => 'repositories#purches', as: :repositories
